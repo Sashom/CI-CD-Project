a4414bbe2d8eaca8bdd60e015684bb4303bdf160
Sashom <mardirossian@gmail.com>
2018-11-06 10:38:11 +0200

    Initial Commit

---
+++ CI-CD Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI-CD Project
+//
+//  Created by Sasho1 on 6.11.18.
+//  Copyright © 2018 WebMaster Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
